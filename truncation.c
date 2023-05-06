4bc4f516a15b6c48c3be356897fede6ece610395
TomasNagy <tomi.nagy@outlook.com>
2023-05-06 20:16:09 +0200

    Add comments

--- truncation.c
+++ truncation.c
@@ -1,6 +1,35 @@
+#include <cs50.h>
 #include <stdio.h>
-#include <cs50.c>
 
+//In this example the numbers are integers
+//If we try to divide 3 by 2, the program should print 1.5
+//But because of integer division it will truncate the number to the integer part = 1
+// int main(void)
+// {
+// 	int a = get_int("a= ");
+// 	int b = get_int("b= ");
+
+// 	// type casting
+// 	int x = a / b;
+
+// 	printf("a / b = %i", x);
+// }
+
+
+//We can make it work with type casting at least one of the numbers to float with (float)
+//Then save the calculated value in a variable declared as float
+// int main(void)
+// {
+// 	int a = get_int("a= ");
+// 	int b = get_int("b= ");
+
+// 	// type casting
+// 	float x = (float) a / b;
+
+// 	printf("a / b = %f", x);
+// }
+
+//If we want more precision we have to use double type
 int main(void)
 {
 	int a = get_int("a= ");
